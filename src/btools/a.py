264d934aeb3b9bcf31c93301aff10cb64aed4de9
Bosco Ho <apposite@gmail.com>
2024-06-26 17:25:31 +1000

    upgrade a

--- src/btools/a.py
+++ src/btools/a.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 
 from __future__ import print_function
-import sys
-import os
+
 import glob
+import os
+import sys
+
+from path import Path
 
 __doc__ = "Quick open applications in Darwin"
 
-applications_dir = '/Applications/'
+home = str(Path.home()) 
+search_dirs = ['/Applications/', f'{home}/MyApps'] 
 
+print(search_dirs) 
 
 def get_name(f):
     base = os.path.basename(f)
     return base.replace('.app', '').lower()
 
-
+def find_match(apps, use_app): 
+    for app in apps: 
+        if use_app in app: 
+            return app 
+        return None 
+    
 def main():
     if len(sys.argv) < 2:
         print("Open OSX apps (searches for text fit to App name)")
@@ -23,13 +33,12 @@ def main():
         params = sys.argv[1:]
 
         use_app = params[0].lower()
-
-        apps = glob.glob(os.path.join(applications_dir, '*'))
-        apps = [get_name(a) for a in apps]
-
-        for app in apps:
-            if use_app in app:
-                params[0] = app
+        for search_dir in search_dirs: 
+            fnames = glob.glob(os.path.join(search_dir, '*'))
+            apps = [get_name(f) for f in fnames]
+            if match_app := find_match(apps, use_app): 
+                params[0] = match_app 
+                break
 
         params = ['"%s"' % p for p in params]
         cmd = 'open -a ' + ' '.join(params)
