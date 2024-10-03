0d25d95b5b949929b1c5db44fce79d2ee4d3f4c0
Bosco Ho <apposite@gmail.com>
2024-10-03 11:05:57 +1000

    fix a tool matching

--- src/btools/a.py
+++ src/btools/a.py
@@ -10,20 +10,17 @@ from path import Path
 
 __doc__ = "Quick open applications in Darwin"
 
-home = str(Path.home()) 
-search_dirs = ['/Applications/', f'{home}/MyApps'] 
-
-print(search_dirs) 
+search_dirs = ['/Applications/', f'{Path.home()}/MyApps']
 
 def get_name(f):
     base = os.path.basename(f)
     return base.replace('.app', '').lower()
 
 def find_match(apps, use_app): 
-    for app in apps: 
+    for app in apps:
         if use_app in app: 
             return app 
-        return None 
+    return None
     
 def main():
     if len(sys.argv) < 2:
@@ -36,11 +33,11 @@ def main():
         for search_dir in search_dirs: 
             fnames = glob.glob(os.path.join(search_dir, '*'))
             apps = [get_name(f) for f in fnames]
-            if match_app := find_match(apps, use_app): 
-                params[0] = match_app 
+            if match_app := find_match(apps, use_app):
+                params[0] = match_app
                 break
 
-        params = ['"%s"' % p for p in params]
+        params = [f'"{p}"' for p in params]
         cmd = 'open -a ' + ' '.join(params)
 
         print(cmd)
